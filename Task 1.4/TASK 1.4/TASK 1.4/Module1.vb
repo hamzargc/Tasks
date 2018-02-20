672e83ec86c42b227a0651170e7a15b18ae05932
hamzargc <34453653+hamzargc@users.noreply.github.com>
2018-02-20 04:39:55 -0800

    Add files via upload

---
+++ Task 1.4/TASK 1.4/TASK 1.4/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+
+    Sub Main()
+
+        Dim MemberID, Newmembers As Integer
+        Dim Name As String
+
+
+        Console.WriteLine("Enter Number Of New Members : ")
+        NewMembers = Console.ReadLine
+
+        For Count = 1 To Newmembers
+
+
+            Console.Write("Enter MemberID: ")
+            MemberID = Console.ReadLine
+
+            Console.WriteLine("Enter Member name: ")
+            Name = Console.ReadLine
+
+
+            FileOpen(1, "C:\HamzaPROJECTS\Members.txt", OpenMode.Append)
+
+            WriteLine(1, MemberID)
+            WriteLine(1, Name)
+
+            FileClose(1)
+
+        Next
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ Task 1.5/Task 1.5/Task 1.5/Module1.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MemberID, Members As Integer
+        Dim Telephone As String
+        Dim JoinDate As Date
+
+
+        Console.WriteLine("Enter Number Of Members : ")
+        Members = Console.ReadLine
+
+        For Count = 1 To Members
+
+
+            Console.Write("Enter MemberID: ")
+            MemberID = Console.ReadLine
+
+
+
+            Console.WriteLine("Enter Member Cell Number: ")
+            Telephone = Console.ReadLine
+
+            Console.WriteLine("Enter Member Join Date: ")
+            JoinDate = Console.ReadLine
+
+
+            FileOpen(1, "C:\HamzaPROJECTS\Task 1.1\Members.txt", OpenMode.Append)
+
+            WriteLine(1, MemberID)
+            WriteLine(1, Telephone)
+            WriteLine(1, JoinDate)
+
+            FileClose(1)
+
+        Next
+
+        Console.ReadKey()
+    End Sub
+
+End Module
