c4b213e0d76df2ad4c25dd9c1e60f42a685f1759
hamzargc <34453653+hamzargc@users.noreply.github.com>
2018-02-20 04:16:10 -0800

    Add files via upload

---
+++ Task 1.1/Task 1.1/Task 1.1/Module1.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MemberID, NofMembers As Integer
+        Dim Name As String
+        
+
+        Console.WriteLine("Enter Number Of Members : ")
+        NofMembers = Console.ReadLine
+
+        For Count = 1 To NofMembers
+
+
+            Console.Write("Enter MemberID: ")
+            MemberID = Console.ReadLine
+
+            Console.WriteLine("Enter Member name: ")
+            Name = Console.ReadLine
+
+
+            FileOpen(1, "C:\HamzaPROJECTS\Members.txt", OpenMode.Append)
+
+            WriteLine(1, MemberID)
+            WriteLine(1, Name)
+
+            FileClose(1)
+
+        Next
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ Task 1.2/1.2/ConsoleApplication1/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MemberID As Integer
+        Dim Name As String
+
+
+        FileOpen(1, "C:\HamzaPROJECTS\Members.txt", OpenMode.Input)
+
+        While Not EOF(1)
+            Input(1, MemberID)
+            Input(1, Name)
+        
+            Console.WriteLine("Member ID is. :" & MemberID)
+            Console.WriteLine("Member name :" & Name)
+      
+        End While
+        FileClose(1)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
