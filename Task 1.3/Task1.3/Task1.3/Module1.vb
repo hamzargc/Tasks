0e974b98bb1be36294afe4b484ae306a26022c50
hamzargc <34453653+hamzargc@users.noreply.github.com>
2018-02-20 04:20:30 -0800

    Add files via upload

---
+++ Task 1.3/Task1.3/Task1.3/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim MemberID As Integer
+        Dim Name, searchN As String
+
+        Console.Write("Enter Name to search for: ")
+        searchN = Console.ReadLine
+
+        FileOpen(1, "C:\HamzaPROJECTS\Task 1.1\Members.txt", OpenMode.Input)
+
+        While Not EOF(1)
+            Input(1, MemberID)
+            Input(1, Name)
+
+
+            If Name = searchN Then
+                Console.WriteLine("Member ID. :" & MemberID)
+                Console.WriteLine("Member name :" & Name)
+            End If
+        End While
+        FileClose(1)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
